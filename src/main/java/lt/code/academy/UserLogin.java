6dac92d87d2ec4c243a99fbfe3ec3eb86095d97f
gamblerLt <rimas.spakauskas@outlook.com>
2023-02-08 20:26:33 +0200

    userLogin pradzia

---
+++ src/main/java/lt/code/academy/UserLogin.java
@@ -0,0 +1,18 @@
+package lt.code.academy;
+
+public class UserLogin {
+
+    public static void main(String[] args) {
+
+        UserLogin userLogin = new UserLogin();
+    }
+
+    private void menu() {
+
+        System.out.println("""
+                1. Prisijungti.
+                2. Atspausdinti visus vartotojus.
+                3. Baigti darbą.
+                """);
+    }
+}
